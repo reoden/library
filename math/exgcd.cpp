c6c13596960d53d4c95edd52ea7bade624e8d131
reoden <staraino_o@163.com>
2023-08-10 13:34:09 +0800

    modified:   math/exgcd.cpp

--- math/exgcd.cpp
+++ math/exgcd.cpp
@@ -1,24 +1,45 @@
-int exgcd(int a, int b, int& x, int& y) {
-    if (b == 0) {
-        x = 1, y = 0;
-        return a;
-    }
+#include <utility>
 
-    int xx, yy;
-    int d = exgcd(b, a % b, xx, yy);
-    x = yy;
-    y = xx - (a / b) * yy;
-    /*
-        int d = exgcd(b, a % b, y, x);
-        y -= (a / b) * x;
-        return d;
-    */
-    return d;
+using std::pair;
+
+const long long safe_mod(long long x, long long m) {
+    x %= m;
+    if (x < 0) x += m;
+    return x;
 }
-/*
-    int d = exgcd(a, b, x, y);
-    assert(a * x + b * y == d);
-    此时x y为 方程组的两个一般解
-    调整x y大小则 x += b / d, y -= a / d;
-    x0 = x + b * t, y0 = (d - a * x) / b;   这个是通解
-*/
+
+// @param b `1 <= b`
+// @return pair(g, x) s.t. g = gcd(a, b), xa = g (mod b), 0 <= x < b/g
+const std::pair<long long, long long> inv_gcd(long long a, long long b) {
+    a = safe_mod(a, b);
+    if (a == 0) return {b, 0};
+
+    // Contracts:
+    // [1] s - m0 * a = 0 (mod b)
+    // [2] t - m1 * a = 0 (mod b)
+    // [3] s * |m1| + t * |m0| <= b
+    long long s = b, t = a;
+    long long m0 = 0, m1 = 1;
+
+    while (t) {
+        long long u = s / t;
+        s -= t * u;
+        m0 -= m1 * u;  // |m1 * u| <= |m1| * s <= b
+
+        // [3]:
+        // (s - t * u) * |m1| + t * |m0 - m1 * u|
+        // <= s * |m1| - t * u * |m1| + t * (|m0| + |m1| * u)
+        // = s * |m1| + t * |m0| <= b
+
+        auto tmp = s;
+        s = t;
+        t = tmp;
+        tmp = m0;
+        m0 = m1;
+        m1 = tmp;
+    }
+    // by [3]: |m0| <= b/g
+    // by g != b: |m0| < b/g
+    if (m0 < 0) m0 += b / s;
+    return {s, m0};
+}
