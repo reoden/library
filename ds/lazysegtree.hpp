41c4fa4829962c1c72694ab4e38ddb4b53382a34
reoden <staraino_o@163.com>
2023-08-13 12:53:33 +0800

    modified:   ds/lazysegtree.hpp

--- ds/lazysegtree.hpp
+++ ds/lazysegtree.hpp
@@ -1,129 +1,233 @@
-#pragma once
-
 #include <algorithm>
-#include <iterator>
-#include <limits>
-#include <numeric>
+#include <cassert>
+#include <iostream>
 #include <vector>
 
-namespace Lazysegmenttree {
-const static int N = 200010;
-template <class T> constexpr T inf = std::numeric_limits<T>::max() / 2;
-
-// @ range add get range min
-struct Info {
-  int64_t minv;
-
-  Info() : minv(inf<int64_t>) {}
-  Info(int64_t _minv) : minv(_minv) {}
+namespace atcoder {
+// @param n `0 <= n`
+// @return minimum non-negative `x` s.t. `n <= 2**x`
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+ 
+// @param n `1 <= n`
+// @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
+constexpr int bsf_constexpr(unsigned int n) {
+    int x = 0;
+    while (!(n & (1 << x))) x++;
+    return x;
+}
+ 
+// @param n `1 <= n`
+// @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsf(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+ 
+template <class S,
+          S (*op)(S, S),
+          S (*e)(),
+          class F,
+          S (*mapping)(F, S),
+          F (*composition)(F, F),
+          F (*id)()>
+struct lazy_segtree {
+  public:
+    lazy_segtree() : lazy_segtree(0) {}
+    explicit lazy_segtree(int n) : lazy_segtree(std::vector<S>(n, e())) {}
+    explicit lazy_segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        lz = std::vector<F>(size, id());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+ 
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+ 
+    S get(int p) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+ 
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return e();
+ 
+        l += size;
+        r += size;
+ 
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+ 
+        S sml = e(), smr = e();
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+ 
+        return op(sml, smr);
+    }
+ 
+    S all_prod() { return d[1]; }
+ 
+    void apply(int p, F f) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = mapping(f, d[p]);
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+    void apply(int l, int r, F f) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return;
+ 
+        l += size;
+        r += size;
+ 
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+ 
+        {
+            int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+ 
+        for (int i = 1; i <= log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
+    }
+ 
+    template <bool (*g)(S)> int max_right(int l) {
+        return max_right(l, [](S x) { return g(x); });
+    }
+    template <class G> int max_right(int l, G g) {
+        assert(0 <= l && l <= _n);
+        assert(g(e()));
+        if (l == _n) return _n;
+        l += size;
+        for (int i = log; i >= 1; i--) push(l >> i);
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(op(sm, d[l]))) {
+                while (l < size) {
+                    push(l);
+                    l = (2 * l);
+                    if (g(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+ 
+    template <bool (*g)(S)> int min_left(int r) {
+        return min_left(r, [](S x) { return g(x); });
+    }
+    template <class G> int min_left(int r, G g) {
+        assert(0 <= r && r <= _n);
+        assert(g(e()));
+        if (r == 0) return 0;
+        r += size;
+        for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(op(d[r], sm))) {
+                while (r < size) {
+                    push(r);
+                    r = (2 * r + 1);
+                    if (g(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+ 
+    friend std::ostream &operator<<(std::ostream &os, lazy_segtree &ls) {
+        os << "{";
+        for(int i = 0; i < ls._n; i++) os << ls.get(i) << (i == ls._n - 1 ? "" : ", ");
+        return os << "}";
+    }
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+    std::vector<F> lz;
+ 
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, F f) {
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id();
+    }
 };
-
-Info operator+(const Info &a, const Info &b) {
-  Info c;
-  c.minv = std::min(a.minv, b.minv);
-  return c;
+ 
+}  // namespace atcoder
+using namespace atcoder;
+ 
+using S = int;
+using F = int;
+S e() {
+  return 0;
 }
-
-struct Tag {
-  int64_t add;
-
-  Tag() : add(0) {}
-  Tag(int _add) : add(_add) {}
-};
-
-void apply(Info &a, Tag &b, const Tag &c) {
-  a.minv = a.minv + c.add;
-  b.add = b.add + c.add;
+ 
+S op(S a, S b) {
+  return std::max(a, b);
 }
-
-Info info[N << 2];
-Tag tag[N << 2];
-
-void pull(int p) { info[p] = info[p * 2] + info[p * 2 + 1]; }
-
-void build(int p, int l, int r) {
-  if (l == r) {
-    info[p].minv = inf<int64_t>;
-    return;
-  }
-
-  int mid = (l + r) >> 1;
-  build(p * 2, l, mid), build(p * 2 + 1, mid + 1, r);
-  pull(p);
+ 
+S mapping(F f, S x) {
+  return std::max(x, f);
 }
-
-void apply(int p, const Tag &v) { apply(info[p], tag[p], v); }
-
-void push(int p) {
-  apply(p * 2, tag[p]);
-  apply(p * 2 + 1, tag[p]);
-  tag[p] = Tag();
+ 
+F composition(F f, F g) {
+  return std::max(f, g);
 }
-
-void change(int p, int l, int r, int x, const Info &v) {
-  if (l == r) {
-    info[p] = v;
-    return;
-  }
-  int mid = (l + r) >> 1;
-  push(p);
-  if (mid >= x) change(p * 2, l, mid, x, v);
-  else change(p * 2 + 1, mid + 1, r, x, v);
-  pull(p);
-}
-
-void modify(int p, int l, int r, int ln, int rn, const Tag &v) {
-  if (l > rn || r < ln)
-    return;
-  if (l >= ln && r <= rn) {
-    apply(p, v);
-    return;
-  }
-
-  push(p);
-  int mid = (l + r) >> 1;
-  modify(p * 2, l, mid, ln, rn, v);
-  modify(p * 2 + 1, mid + 1, r, ln, rn, v);
-  pull(p);
-}
-
-Info query(int p, int l, int r, int ln, int rn) {
-  if (l > rn || r < ln)
-    return Info();
-  if (l >= ln && r <= rn) {
-    return info[p];
-  }
-
-  push(p);
-  int mid = (l + r) >> 1;
-  return query(p * 2, l, mid, ln, rn) + query(p * 2 + 1, mid + 1, r, ln, rn);
-}
-
-// @ return first index >= v
-int lower(int p, int l, int r, int ln, int rn, const Info &v) {
-  push(p);
-  if (l >= ln && r <= rn) {
-    if (info[p].minv < v.minv)
-      return -1;
-    if (l == r)
-      return l;
-    int mid = (l + r) / 2;
-    if (info[p * 2].minv >= v.minv)
-      return lower(p * 2, l, mid, ln, rn, v);
-    return lower(p * 2 + 1, mid + 1, r, ln, rn, v);
-  }
-  int mid = (l + r) / 2;
-
-  if (rn <= mid)
-    return lower(p * 2, l, mid, ln, rn, v);
-  else if (mid < l)
-    return lower(p * 2 + 1, mid + 1, r, ln, rn, v);
-  else {
-    int pos = lower(p * 2, l, mid, ln, mid, v);
-    if (!~pos)
-      return lower(p * 2 + 1, mid + 1, r, mid + 1, rn, v);
-    return pos;
-  }
-}
-} // namespace Lazysegmenttree
-using namespace Lazysegmenttree;
